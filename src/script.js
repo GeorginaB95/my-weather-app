02351c1b9f5eedf108c7b31c52ff8f472b5264f2
GeorginaB95 <georginabarnsley@outlook.com>
2021-04-09 18:06:47 +0100

    Added forecast API

--- src/script.js
+++ src/script.js
@@ -13,7 +13,8 @@ function formatDate(timestamp){
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast(){
+function displayForecast(response){
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
     let forecastHTML = `<div class="row">`;
     let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -40,6 +41,12 @@ function displayForecast(){
     forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates){
+let apiKey= "d8db460d456f3ec32826f6858578adce";
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response){
 
    let temperatureElement = document.querySelector("#current-temperature");
@@ -58,6 +65,7 @@ function displayTemperature(response){
    dateElement.innerHTML = formatDate(response.data.dt * 1000);
    iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
    iconElement.setAttribute ("alt", response.data.weather[0].description);
+   getForecast(response.data.coord);
 }
 
 function search(city){
